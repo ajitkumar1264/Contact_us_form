4fb9131cfda7a002849159ca514013d2af353202
vaghela Ajitkumar <91899491+ajitkumar1264@users.noreply.github.com>
2022-07-25 22:05:14 +0530

    googlesignin

--- src/component/Signin.js
+++ src/component/Signin.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import {auth} from '../config';
 import {useNavigate} from 'react-router-dom'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {createUserWithEmailAndPassword,GoogleAuthProvider,signInWithPopup,sendEmailVerification} from 'firebase/auth'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,6 +11,33 @@ const [email, setemail] = useState("");
 const [password, setpassword] = useState("");
 const nav=useNavigate();
 
+
+   const googlesign=async()=>{
+
+const provider=new GoogleAuthProvider();
+ await signInWithPopup(auth,provider).then((result)=>{
+  toast.success("succesfully google sign in")
+  
+ const name=result.user.displayName;
+ const Email=result.user.email;
+ const profile=result.user.photoURL;
+
+localStorage.setItem("name",name);
+localStorage.setItem("email",Email);
+localStorage.setItem("photourl",profile);
+
+setTimeout(() => {
+  nav('/home')
+},500);
+
+  
+}).catch(error=>{console.log(error.message)})
+
+sendEmailVerification(auth.currentUser)
+.then(()=>{
+  toast.success('please verify ypur account in gmail');
+})
+}
 
 
 const verify=()=>{
@@ -37,6 +64,10 @@ const verify=()=>{
 <h1 className='text-3xl mt-10 font-medium' > confirm your email - {email}</h1>
 </div>
 
+</div>
+
+<div className='mt-5'>
+<button onClick={googlesign} className='box px-5 font-semibold py-2 ring-2 rounded-lg text-xl  hover:ring-4 hover:bg-sky-300 bg-sky-500 ring-black transform ease-out duration-500'>sign in with the google</button>
 </div>
 
 <ToastContainer
